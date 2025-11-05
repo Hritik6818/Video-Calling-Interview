ea0f25a02e3648a9e8220c45d9662d3d6aa000c8
Hritik Shukla <Hritik Shukla>
2025-11-05 15:50:05 +0530

    deployment config adding

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -1,12 +1,31 @@
 import express from 'express';
+import path from 'path'
 import {ENV} from './lib/env.js'
+
+
 const app = express();
+const __dirname = path.resolve()
 
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
+
+
 app.get("/",(req,res) => {
   res.status(200).json({msg:"Success From Bad Habit"})
 })
+
+app.get("/Home",(req,res) => {
+  res.status(200).json({msg:"Hello How Are You"})
+})
+
+if(ENV.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("/{*any}",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  })
+}
+
 
 app.listen(ENV.PORT,() => {
   console.log(`Server is running on port ${ENV.PORT}`)
